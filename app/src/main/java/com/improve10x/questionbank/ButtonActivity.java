de9a77727ae222a7871556e4accceda536e1486f
CHARAN TEJA <charan.teja@improve10x.com>
2023-02-10 14:41:39 +0530

    added ouestion 2, question 3 buttons and given functunality to that buttons

--- app/src/main/java/com/improve10x/questionbank/ButtonActivity.java
+++ app/src/main/java/com/improve10x/questionbank/ButtonActivity.java
@@ -2,13 +2,64 @@ package com.improve10x.questionbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ButtonActivity extends AppCompatActivity {
+    Button questionOneBtn;
+    Button questionTwoBtn;
+    Button questionThreeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
+        handleInitViews();
+        handleQuestionnOneBtn();
+        handleQuestion2Btn();
+        handleQuestion3Btn();
+    }
+
+    private void handleQuestion3Btn() {
+      questionTwoBtn.setOnClickListener(v -> {
+          String question = "Given a = 10, b = 20, return true the sum of both numbers is less than hundred  otherwise return false";
+          String optionA = "true";
+          String optionB = "false";
+          String optionC = "Non of above";
+          String optionD = "Both a and b";
+          navigateToQuestionsActivity(question, optionA, optionB, optionC, optionD);
+      });
+    }
+
+    private void handleQuestion2Btn() {
+
+    }
+
+    private void handleQuestionnOneBtn() {
+        questionOneBtn.setOnClickListener(view -> {
+            String question1 = "How many palnets are there in solar syatem";
+            String optionA = " 10 ";
+            String optionB = " 9 ";
+            String optionC = " 12 ";
+            String optionD = " 8 ";
+            navigateToQuestionsActivity(question1, optionA, optionB, optionC, optionD);
+        });
+    }
+
+    public void navigateToQuestionsActivity(String question1, String optionA, String optionB, String optionC, String optiond) {
+        Intent questionsIntent = new Intent(this, QuestionsActivity.class);
+        questionsIntent.putExtra("question", question1);
+        questionsIntent.putExtra("optioon1", optionA);
+        questionsIntent.putExtra("optioon2", optionB);
+        questionsIntent.putExtra("optioon3", optionC);
+        questionsIntent.putExtra("optioon4", optiond);
+        startActivity(questionsIntent);
+    }
+
+    private void handleInitViews() {
+        questionOneBtn = findViewById(R.id.question_one_btn);
+        questionTwoBtn = findViewById(R.id.question_two_btn);
+        questionThreeBtn = findViewById(R.id.question_three_btn);
     }
 }
--- app/src/main/java/com/improve10x/questionbank/QuestionsActivity.java
+++ app/src/main/java/com/improve10x/questionbank/QuestionsActivity.java
@@ -2,13 +2,48 @@ package com.improve10x.questionbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuestionsActivity extends AppCompatActivity {
+    TextView questionTxt;
+    RadioGroup optionsRg;
+    RadioButton optionOneRb;
+    RadioButton optionTwoRb;
+    RadioButton optionThreeRb;
+    RadioButton optionFourRb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+        handleInitViews();
+        Intent intent = getIntent();
+        String question = intent.getStringExtra("question");
+        String optionA = intent.getStringExtra("optioon1");
+        String optionB = intent.getStringExtra("optioon2");
+        String optionC = intent.getStringExtra("optioon3");
+        String optionD = intent.getStringExtra("optioon4");
+        questionTxt.setText(question);
+        optionOneRb.setText(optionA);
+        optionTwoRb.setText(optionB);
+        optionThreeRb.setText(optionC);
+        optionFourRb.setText(optionD);
+
+
+    }
+
+    private void handleInitViews() {
+        questionTxt = findViewById(R.id.question_txt);
+        optionsRg = findViewById(R.id.options_rg);
+        optionOneRb = findViewById(R.id.option_one_rb);
+        optionTwoRb = findViewById(R.id.option_two_rb);
+        optionThreeRb = findViewById(R.id.option_three_rb);
+        optionFourRb = findViewById(R.id.option_four_rb);
+
     }
 }
